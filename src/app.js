fa0be8dbf5f9bf668460ef63c874033760033973
mucahitcinar <mucocinarmuco@gmail.com>
2021-10-20 21:00:57 +0300

    20 ekim 2021 ders sonu

---
+++ src/app.js
@@ -0,0 +1,12 @@
+let konutKredileri=["Konut kredisi","Emlak Konuut Kredisi","Kamu konut kredisi"]
+
+console.log("<ul>")
+
+for(let i=0;i<3;i++)
+{
+    console.log("<li>"+konutKredileri[i]+"<li>")
+}
+
+
+console.log("</ul>")
+
